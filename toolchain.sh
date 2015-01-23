bde275e064a6ac3a7387300e97dd6eb40ab3f8ce
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2015-01-23 19:18:46 +0100

    Update toolchain.sh

--- toolchain.sh
+++ toolchain.sh
@@ -1,3 +1,7 @@
 google docs get --title='Virtuelle Flipper bauen' --dest=vpin --format=docx
 pandoc -s -S -t docbook vpin.docx -o vpin.db
+xmlstarlet ed -L -d "//sect1[@id='allgemeine-hinweise-für-alle-autoren']" vpin.db
+xmlstarlet ed -L -d "//sect1[@id='inhaltsverzeichnis']" vpin.db
 xmlto xhtml -m config.xsl vpin.db -o html/
+unzip -d tmp vpin.docx
+mv tmp/word/media html/
