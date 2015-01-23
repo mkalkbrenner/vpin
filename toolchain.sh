fa2cb1eecc93570517c267c4ccb29659eaf4ef23
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2015-01-23 19:30:38 +0100

    added stylesheets

--- toolchain.sh
+++ toolchain.sh
@@ -1,7 +1,10 @@
+rm html/*.html
+rm -rf html/media
+rm -rf tmp
 google docs get --title='Virtuelle Flipper bauen' --dest=vpin --format=docx
 pandoc -s -S -t docbook vpin.docx -o vpin.db
 xmlstarlet ed -L -d "//sect1[@id='allgemeine-hinweise-für-alle-autoren']" vpin.db
 xmlstarlet ed -L -d "//sect1[@id='inhaltsverzeichnis']" vpin.db
-xmlto xhtml -m config.xsl vpin.db -o html/
+xmlto xhtml -m vpin.xsl vpin.db -o html/
 unzip -d tmp vpin.docx
 mv tmp/word/media html/
