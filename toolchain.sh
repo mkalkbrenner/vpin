8bfc005b5fd8a4778aa52d57705f9a41c33da606
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2015-01-23 20:33:17 +0100

    improved docbook

--- toolchain.sh
+++ toolchain.sh
@@ -1,10 +1,27 @@
+# Clean up last run.
 rm html/*.html
 rm -rf html/media
 rm -rf tmp
+
+# Fetch the current document form google as docx. Pandoc produces a better result from docx compared to odf.
 google docs get --title='Virtuelle Flipper bauen' --dest=vpin --format=docx
+
+# Convert the docx to docbook.
 pandoc -s -S -t docbook vpin.docx -o vpin.db
+
+# Remove the non content sections from the document.
 xmlstarlet ed -L -d "//sect1[@id='allgemeine-hinweise-für-alle-autoren']" vpin.db
 xmlstarlet ed -L -d "//sect1[@id='inhaltsverzeichnis']" vpin.db
+
+# Convert some style elements we use at google docs into real docbook elements. 
+xmlstarlet ed -L -r '//blockquote' -v 'warning' vpin.db
+xmlstarlet ed -L -r '//para[emphasis[not(@*)]]' -v 'blockquote' vpin.db
+xmlstarlet ed -L -r '//blockquote/emphasis' -v 'para' vpin.db
+
+# Create a set of html pages from the docbook format.
 xmlto xhtml -m vpin.xsl vpin.db -o html/
+
+# Copy the images form the original docx to the html folder.
 unzip -d tmp vpin.docx
 mv tmp/word/media html/
+
--- vpin.xsl
+++ vpin.xsl
@@ -5,5 +5,9 @@
   <xsl:param name="use.id.as.filename" select="'1'"/>
   <xsl:param name="admon.graphics" select="'1'"/>
   <xsl:param name="admon.graphics.path"></xsl:param>
-  <xsl:param name="html.stylesheet" select="'../docbook.css'"/>
+  <xsl:param name="html.stylesheet" select="'vpin.css'"/>
+  <xsl:param name="section.autolabel" select="1"/>
+  <xsl:param name="section.label.includes.component.label" select="1"/>
+  <xsl:param name="chunk.section.depth" select="2"/>
+  <xsl:param name="chunk.first.sections" select="1"/>
 </xsl:stylesheet>
